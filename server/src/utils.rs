aaca81c751e4d8815cd3ae6451a35729886c4c01
BishwasXD <gautambishwas7@gmail.com>
2024-10-20 10:34:14 +0545

    rendered html template

---
+++ server/src/utils.rs
@@ -0,0 +1,9 @@
+//contains fns that returns type of request and request body.
+
+pub fn request_type(){
+
+}
+
+pub fn request_data(){
+    
+}
---
+++ server/src/views.rs
@@ -0,0 +1,6 @@
+//for handling logic
+ 
+pub fn views(){
+
+
+}
